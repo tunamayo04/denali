cd73a512034740127a6d70e1f94febac6de41387
Emma Plante <emmaplante04@gmail.com>
2026-06-19 00:56:46 -0400

    Added budget row query

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -3,55 +3,59 @@ use axum::{
     http::StatusCode,
     Json, Router,
 };
+use axum::extract::Query;
+use axum::http::Method;
+use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
+use tower::ServiceBuilder;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
     // initialize tracing
     tracing_subscriber::fmt::init();
 
-    // build our application with a route
+    let cors = CorsLayer::new()
+    .allow_methods(vec![Method::GET, Method::POST, Method::PUT, Method::DELETE])
+    .allow_origin(Any);
+
+    let service = ServiceBuilder::new()
+        .layer(cors);
+
     let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root))
-        // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .route("/GetBudgetRows", get(get_budget_rows))
+        .layer(service);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await;
+    axum::serve(listener, app).await.unwrap();
 }
 
-// basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn get_budget_rows(Query(params): Query<GetBudgetItemsRequest>) -> (StatusCode, Json<Vec<BudgetItem>>) {
+    println!("{}", params.year);
+    println!("{}", params.month);
+    let rows = vec![
+        BudgetItem {
+            category: String::from("Rent"),
+            budget: Decimal::from(1750),
+            actual: Decimal::from(1750),
+            color: String::from("#0084FF")
+        }
+    ];
+
+    (StatusCode::OK, Json(rows))
 }
 
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
-// the input to our `create_user` handler
 #[derive(Deserialize)]
-struct CreateUser {
-    username: String,
+struct GetBudgetItemsRequest {
+    year: u16,
+    month: u16,
 }
 
-// the output to our `create_user` handler
 #[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
+struct BudgetItem {
+    category: String,
+    budget: Decimal,
+    actual: Decimal,
+    color: String,
 }
